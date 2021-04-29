6692faaf2bbdc6e38b953d4ad5f01174a22562fd
Johnathon Hall <jhall41@my.wctc.edu>
2021-04-29 14:06:21 -0500

    Initial

--- src/main/java/edu/wctc/distjavazodiac/service/BasicMonthListService.java
+++ src/main/java/edu/wctc/distjavazodiac/service/BasicMonthListService.java
@@ -2,6 +2,8 @@ package edu.wctc.distjavazodiac.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.wctc.distjavazodiac.entity.Month;
+import edu.wctc.distjavazodiac.repo.MonthRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,21 +16,30 @@ import java.util.List;
 @Service
 public class BasicMonthListService implements MonthListService {
     private List<Month> monthList;
+    @Autowired
+    private MonthRepository monthRepository;
 
     @Override
-    public List<Month> getMonths() {
-        return monthList;
+    public List<Month> getMonths(){
+        List<Month> list = new ArrayList<>();
+        monthRepository.findAll().forEach(list::add);
+        return list;
     }
 
-    @PostConstruct
-    public void initMonths() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            Month[] monthArray = mapper.readValue(Paths.get("months.json").toFile(), Month[].class);
-            monthList = Arrays.asList(monthArray);
-        } catch (IOException e) {
-            e.printStackTrace();
-            monthList = new ArrayList<>(0);
-        }
-    }
+//    @Override
+//    public List<Month> getMonths() {
+//        return monthList;
+//    }
+//
+//    @PostConstruct
+//    public void initMonths() {
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            Month[] monthArray = mapper.readValue(Paths.get("months.json").toFile(), Month[].class);
+//            monthList = Arrays.asList(monthArray);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            monthList = new ArrayList<>(0);
+//        }
+//    }
 }
--- src/main/java/edu/wctc/distjavazodiac/service/RandomHoroscopeService.java
+++ src/main/java/edu/wctc/distjavazodiac/service/RandomHoroscopeService.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.wctc.distjavazodiac.entity.Birthday;
 import edu.wctc.distjavazodiac.entity.Fortune;
 import edu.wctc.distjavazodiac.entity.Horoscope;
+import edu.wctc.distjavazodiac.repo.FortuneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,10 +23,13 @@ public class RandomHoroscopeService implements HoroscopeService {
     private List<Fortune> allFortunes;
     private ZodiacService zodiacService;
 
+
     @Autowired
     public RandomHoroscopeService(ZodiacService zodiacService) {
         this.zodiacService = zodiacService;
     }
+    @Autowired
+    private FortuneRepository fortuneRepository;
 
     @Override
     public Horoscope getHoroscope(Birthday birthday) {
@@ -37,23 +42,25 @@ public class RandomHoroscopeService implements HoroscopeService {
 
         Horoscope hscope = new Horoscope();
         hscope.setSign(sign);
+        allFortunes = new ArrayList<>();
+        fortuneRepository.findAll().forEach(allFortunes::add);
 
         int randomIndex = (int) (Math.random() * allFortunes.size());
         hscope.setHoroscope(allFortunes.get(randomIndex).getText());
         return hscope;
     }
 
-    @PostConstruct
-    public void initHoroscopes() {
-        ObjectMapper mapper = new ObjectMapper();
-
-        Fortune[] fortuneArray;
-        try {
-            fortuneArray = mapper.readValue(Paths.get("fortunes.json").toFile(), Fortune[].class);
-        } catch (IOException e) {
-            e.printStackTrace();
-            fortuneArray = new Fortune[]{new Fortune()};
-        }
-        allFortunes = Arrays.asList(fortuneArray);
-    }
+//    @PostConstruct
+//    public void initHoroscopes() {
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        Fortune[] fortuneArray;
+//        try {
+//            fortuneArray = mapper.readValue(Paths.get("fortunes.json").toFile(), Fortune[].class);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            fortuneArray = new Fortune[]{new Fortune()};
+//        }
+//        allFortunes = Arrays.asList(fortuneArray);
+//    }
 }
